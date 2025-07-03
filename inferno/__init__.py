b50565ea79fc6c177ebfcb185b39e59c264fcd3b
Jonathan Wenger <jw4246@columbia.edu>
2025-07-03 11:50:30 -0400

    retrieve version at runtime

--- inferno/__init__.py
+++ inferno/__init__.py
@@ -1,5 +1,13 @@
 """Bayesian Neural Networks in PyTorch."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from . import bnn, datasets, loss_fns, models
 
 __all__ = ["bnn", "datasets", "loss_fns", "models"]
+
+try:
+    __version__ = version("inferno")
+except PackageNotFoundError:
+    # package is not installed
+    pass

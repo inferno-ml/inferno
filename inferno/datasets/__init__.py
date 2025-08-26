78c1a55f9e5ec09951a8f2154dd37815bfcf7fae
Jonathan Wenger <jw4246@columbia.edu>
2025-08-26 16:23:53 -0400

    Additional tabular datasets (Wine & Concrete) (#3)

--- inferno/datasets/__init__.py
+++ inferno/datasets/__init__.py
@@ -1,6 +1,5 @@
 from .regression_dataset import RegressionDataset  # isort:skip
-from .tabular import ParkinsonsTelemonitoring, ProteinStructure, RoadNetwork
-from .vision import (
+from .image import (
     CIFAR10,
     CIFAR10C,
     CIFAR100,
@@ -9,4 +8,11 @@ from .vision import (
     MNISTC,
     TinyImageNet,
     TinyImageNetC,
+)
+from .tabular import (
+    ConcreteCompressiveStrength,
+    ParkinsonsTelemonitoring,
+    ProteinStructure,
+    RoadNetwork,
+    WineQuality,
 )

a9bf28805fff06a800908bc957f95fd8153d8196
Beau Coker <beaucoker@Beaus-MacBook-Pro-2.local>
2025-09-02 14:08:27 -0400

    New tests for allowed/prohibited torch modules

--- tests/bnn/modules/test_bnn_mixin.py
+++ tests/bnn/modules/test_bnn_mixin.py
@@ -67,3 +67,55 @@ def test_mixin_allows_setting_parametrization(
     my_bnn_module = MyBNNModule(**kwargs, parametrization=parametrization)
 
     assert isinstance(my_bnn_module.parametrization, parametrization.__class__)
+
+
+@pytest.mark.parametrize(
+    "TorchClass,kwargs",
+    [
+        (nn.Linear, {"in_features": 5, "out_features": 2}),
+        (nn.Conv1d, {"in_channels": 3, "out_channels": 1, "kernel_size": 1}),
+    ],
+)
+def test_mixin_prohibits_certain_torch_modules(
+    TorchClass: nn.Module, kwargs: dict
+):
+
+    # Check that reset_parameters and parameters_and_lrs fail when TorchClass used
+    class MyBNNModule(bnn.BNNMixin, nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.layer = TorchClass(**kwargs)
+
+    my_bnn_module = MyBNNModule()
+
+    with pytest.raises(NotImplementedError):
+        my_bnn_module.reset_parameters()
+
+    with pytest.raises(NotImplementedError):
+        my_bnn_module.parameters_and_lrs(lr=1.0, optimizer="SGD")
+
+
+
+@pytest.mark.parametrize(
+    "TorchClass,kwargs",
+    [
+        (nn.LayerNorm, {"normalized_shape": (3,2)}), 
+        (nn.GroupNorm, {"num_groups": 2, "num_channels": 4}),
+        (nn.BatchNorm1d, {"num_features": 3}),
+        (nn.BatchNorm2d, {"num_features": 3}),
+        (nn.BatchNorm3d, {"num_features": 3}),
+    ],
+)
+def test_mixin_allows_certain_torch_modules(
+    TorchClass: nn.Module, kwargs: dict
+):
+
+    # Check that reset_parameters and parameters_and_lrs work when TorchClass used
+    class MyBNNModule(bnn.BNNMixin, nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.layer = TorchClass(**kwargs)
+
+    my_bnn_module = MyBNNModule()
+    my_bnn_module.reset_parameters()
+    my_bnn_module.parameters_and_lrs(lr=1.0, optimizer="SGD")

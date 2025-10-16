acd551a06fe4bf32f4e69093a8f5f8728fa8cb76
Jonathan Wenger <jw4246@columbia.edu>
2025-10-16 10:14:04 -0400

    import fixed

--- inferno/bnn/modules/sin_pos_encoding.py
+++ inferno/bnn/modules/sin_pos_encoding.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import math
 from typing import TYPE_CHECKING
 
 import torch
-from torch import Tensor, nn
+from torch import nn
 
 if TYPE_CHECKING:
     from jaxtyping import Float

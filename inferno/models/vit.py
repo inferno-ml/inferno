b335e894f8f03f6bae71683b19035abd63e6c529
Beau Coker <beaucoker@Beaus-MacBook-Pro-7.local>
2025-12-29 15:03:27 -0500

    switching to load_state_dict from MLPBlock

--- inferno/models/vit.py
+++ inferno/models/vit.py
@@ -17,8 +17,9 @@ from torchvision.transforms._presets import ImageClassification, InterpolationMo
 from torchvision.utils import _log_api_usage_once
 
 # if TYPE_CHECKING:
-from .. import bnn, models
+from .. import bnn
 from ..bnn import params
+from ..models import MLP
 
 
 class ConvStemConfig(NamedTuple):
@@ -27,74 +28,6 @@ class ConvStemConfig(NamedTuple):
     stride: int
     norm_layer: Callable[..., nn.Module] = nn.BatchNorm2d
     activation_layer: Callable[..., nn.Module] = nn.ReLU
-
-
-"""
-From [``torchvision.ops.misc.MLP``] https://github.com/pytorch/vision/blob/main/torchvision/ops/misc.py
-"""
-
-
-class MLP(bnn.Sequential):
-    """This block implements the multi-layer perceptron (MLP) module.
-
-    Args:
-        in_channels (int): Number of channels of the input
-        hidden_channels (List[int]): List of the hidden channel dimensions
-        norm_layer (Callable[..., torch.nn.Module], optional): Norm layer that will be stacked on top of the linear layer. If ``None`` this layer won't be used. Default: ``None``
-        activation_layer (Callable[..., torch.nn.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the linear layer. If ``None`` this layer won't be used. Default: ``torch.nn.ReLU``
-        inplace (bool, optional): Parameter for the activation layer, which can optionally do the operation in-place.
-            Default is ``None``, which uses the respective default values of the ``activation_layer`` and Dropout layer.
-        bias (bool): Whether to use bias in the linear layer. Default ``True``
-        dropout (float): The probability for the dropout layer. Default: 0.0
-    """
-
-    def __init__(
-        self,
-        in_channels: int,
-        hidden_channels: list[int],
-        norm_layer: Optional[Callable[..., torch.nn.Module]] = None,
-        activation_layer: Optional[Callable[..., torch.nn.Module]] = torch.nn.ReLU,
-        inplace: Optional[bool] = None,
-        bias: bool = True,
-        dropout: float = 0.0,
-        parametrization: params.Parametrization = params.MaximalUpdate(),
-        cov: params.FactorizedCovariance | None = None,
-    ):
-        # The addition of `norm_layer` is inspired from the implementation of TorchMultimodal:
-        # https://github.com/facebookresearch/multimodal/blob/5dec8a/torchmultimodal/modules/layers/mlp.py
-        params = {} if inplace is None else {"inplace": inplace}
-
-        layers = []
-        in_dim = in_channels
-        for hidden_dim in hidden_channels[:-1]:
-            layers.append(
-                bnn.Linear(
-                    in_dim,
-                    hidden_dim,
-                    bias=bias,
-                    parametrization=parametrization,
-                    cov=cov,
-                )
-            )
-            if norm_layer is not None:
-                layers.append(norm_layer(hidden_dim))
-            layers.append(activation_layer(**params))
-            layers.append(torch.nn.Dropout(dropout, **params))
-            in_dim = hidden_dim
-
-        layers.append(
-            bnn.Linear(
-                in_dim,
-                hidden_channels[-1],
-                bias=bias,
-                parametrization=parametrization,
-                cov=cov,
-            )
-        )
-        layers.append(torch.nn.Dropout(dropout, **params))
-
-        super().__init__(*layers)
-        _log_api_usage_once(self)
 
 
 class MLPBlock(MLP):
@@ -112,7 +45,8 @@ class MLPBlock(MLP):
     ):
         super().__init__(
             in_dim,
-            [mlp_dim, in_dim],
+            [mlp_dim],
+            in_dim,
             activation_layer=nn.GELU,
             inplace=None,
             dropout=dropout,
@@ -121,7 +55,7 @@ class MLPBlock(MLP):
         )
 
         for m in self.modules():
-            if isinstance(m, nn.Linear):
+            if isinstance(m, bnn.Linear):
                 nn.init.xavier_uniform_(m.weight)
                 if m.bias is not None:
                     nn.init.normal_(m.bias, std=1e-6)
@@ -196,22 +130,10 @@ class EncoderBlock(bnn.BNNMixin, nn.Module):
         # MLP block
         self.ln_2 = norm_layer(hidden_dim)
 
-        """
         self.mlp = MLPBlock(
             hidden_dim,
             mlp_dim,
             dropout,
-            cov=cov["mlp"],
-            parametrization=parametrization,
-        )
-        """
-
-        self.mlp = models.MLP(
-            hidden_dim,
-            [mlp_dim],
-            hidden_dim,
-            activation_layer=nn.GELU,
-            dropout=dropout,
             cov=cov["mlp"],
             parametrization=parametrization,
         )
